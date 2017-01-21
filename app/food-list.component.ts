f43176956053b9cb223de6a9ca074263ed2feacc
Andrew Accuardi <andrewaccuardi@gmail.com>
2017-01-20 16:53:07 -0800

    Finish pipe

--- app/food-list.component.ts
+++ app/food-list.component.ts
@@ -5,13 +5,18 @@ import {Food} from './food.model';
   selector: 'food-list',
   template: `
   <div class="container">
+    <select (change)="onChange($event.target.value)">
+      <option value="allFood">All Food</option>
+      <option value="lessThan500">Foods less than 500 calories</option>
+      <option value="moreThan500" selected="selected">Foods more than 500 calories</option>
+    </select><br>
     <table class="table table-striped table-bordered">
       <tr>
         <th>Name</th>
         <th>Calories</th>
         <th>Notes</th>
       </tr>
-      <tr *ngFor="let currentFood of childFoodList">
+      <tr *ngFor="let currentFood of childFoodList | calories:filterByCalories">
         <td (click)="editFood(currentFood)">{{currentFood.name}}</td>
         <td>{{currentFood.calories}}</td>
         <td>{{currentFood.notes}}</td>
@@ -26,10 +31,13 @@ import {Food} from './food.model';
 export class FoodListComponent {
   @Input() childFoodList: Food[];
   selectedFood : boolean = null;
+  filterByCalories: string = 'moreThan500';
 
   editFood(currentFood) {
     this.selectedFood = currentFood;
+  }
 
-
+  onChange(optionFromMenu) {
+    this.filterByCalories = optionFromMenu;
   }
 }
